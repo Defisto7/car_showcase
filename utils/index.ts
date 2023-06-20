a254bd2f603958e5be164728613370f33c815d67
Dima Yanovskji <criska7@gmail.com>
2023-06-20 17:41:06 +0300

    Master
    setting API and add carCard

---
+++ utils/index.ts
@@ -0,0 +1,28 @@
+export async function fetchCars() {
+    const headers = {
+            'X-RapidAPI-Key': 'd09f017bbfmshf6c06f5a2f2bae8p166d69jsn9932b3cba863',
+            'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
+    }
+    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {
+        headers: headers,
+    });
+
+    const result = await response.json();
+
+    return result;
+}
+
+export const calculateCarRent = (city_mpg: number, year: number) => {
+    const basePricePerDay = 50; // Base rental price per day in dollars
+    const mileageFactor = 0.1; // Additional rate per mile driven
+    const ageFactor = 0.05; // Additional rate per year of vehicle age
+  
+    // Calculate additional rate based on mileage and age
+    const mileageRate = city_mpg * mileageFactor;
+    const ageRate = (new Date().getFullYear() - year) * ageFactor;
+  
+    // Calculate total rental rate per day
+    const rentalRatePerDay = basePricePerDay + mileageRate + ageRate;
+  
+    return rentalRatePerDay.toFixed(0);
+  };
